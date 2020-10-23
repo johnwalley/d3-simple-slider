33b6b38cc4b1de2e551c66a9354d9bfd495f086c
mbucknell <mbucknell@usgs.gov>
2020-10-23 11:47:32 -0500

    Import all objects from d3-selection and see if 'event' is one of the properties to determine the version.

--- src/d3-compat.js
+++ src/d3-compat.js
@@ -1,13 +1,14 @@
-import { event } from 'd3-selection';
+import * as selection from 'd3-selection';
 
 export function adaptListener(listener) {
+  const isv6 = !('event' in Object.getOwnPropertyNames(selection));
   return function (a, b) {
-    if (a && a.target) {
+    if (isv6) {
       // d3@v6
       listener.call(this, a, b);
     } else {
       // d3@v5
-      listener.call(this, event, a);
+      listener.call(this, selection.event, a);
     }
   };
 }
