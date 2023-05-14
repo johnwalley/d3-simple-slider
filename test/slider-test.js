5e96c709875e52a95f16bac73dafd4bd08102a68
John Walley <john@walley.org.uk>
2023-05-14 11:51:09 +0100

    feat: support up-to-date d3 versions (#224)

--- test/slider-test.js
+++ test/slider-test.js
@@ -1,96 +1,84 @@
-var tape = require('tape');
-var jsdom = require('jsdom');
-var fs = require('fs');
-var path = require('path');
-d3 = Object.assign({}, require('d3-selection'), require('../'));
+import assert from 'assert';
+import jsdom from 'jsdom';
+import { readFileSync } from 'fs';
+import { resolve, dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
-tape('sliderHorizontal() has the expected defaults', function(test) {
-  var s = d3.sliderHorizontal();
-  test.equal(s.tickFormat(), null);
-  test.end();
+import { select } from 'd3-selection';
+
+import * as d3 from '../src/index.js';
+
+it('sliderHorizontal() has the expected defaults', () => {
+  const s = d3.sliderHorizontal();
+  assert.equal(s.tickFormat(), null);
 });
 
-tape('slider.min(value), slider.max(value) set the expected values', function(
-  test
-) {
-  var s = d3
-    .sliderHorizontal()
-    .min(100)
-    .max(200);
-  test.equal(s.min(), 100);
-  test.equal(s.max(), 200);
-  test.deepEqual(s.domain(), [100, 200]);
-  test.end();
+it('slider.min(value), slider.max(value) set the expected values', () => {
+  const s = d3.sliderHorizontal().min(100).max(200);
+  assert.equal(s.min(), 100);
+  assert.equal(s.max(), 200);
+  assert.deepEqual(s.domain(), [100, 200]);
 });
 
-tape('slider.domain([min,max]) sets the expected values', function(test) {
-  var s = d3.sliderHorizontal().domain([100, 200]);
-  test.deepEqual(s.domain(), [100, 200]);
-  test.equal(s.min(), 100);
-  test.equal(s.max(), 200);
-  test.end();
+it('slider.domain([min,max]) sets the expected values', () => {
+  const s = d3.sliderHorizontal().domain([100, 200]);
+  assert.deepEqual(s.domain(), [100, 200]);
+  assert.equal(s.min(), 100);
+  assert.equal(s.max(), 200);
 });
 
-tape('slider.default(value) sets the default value', function(test) {
-  var s = d3.sliderHorizontal().default(10);
-  test.equal(s.value(), 10);
-  test.equal(s.default(), 10);
-  test.end();
+it('slider.default(value) sets the default value', () => {
+  const s = d3.sliderHorizontal().default(10);
+  assert.equal(s.value(), 10);
+  assert.equal(s.default(), 10);
 });
 
-tape('slider.default(value) sets the default range', function(test) {
-  var s = d3.sliderHorizontal().default([4, 8]);
-  test.deepEqual(s.value(), [4, 8]);
-  test.deepEqual(s.default(), [4, 8]);
-  test.end();
+it('slider.default(value) sets the default range', () => {
+  const s = d3.sliderHorizontal().default([4, 8]);
+  assert.deepEqual(s.value(), [4, 8]);
+  assert.deepEqual(s.default(), [4, 8]);
 });
 
-tape('sliderVertical(selection) produces the expected result', function(test) {
-  var window = new jsdom.JSDOM('<!DOCTYPE html><svg><g></g></svg>').window;
+it('sliderHorizontal(selection) produces the expected result', () => {
+  const window = new jsdom.JSDOM('<!DOCTYPE html><svg><g></g></svg>').window;
   global.window = window;
   global.document = window.document;
   global.navigator = {
     userAgent: 'node.js',
   };
   copyProps(window, global);
-  var bodyActual = window.document.body;
-  var bodyExpected = new jsdom.JSDOM(file('slider-horizontal.html')).window
+  const bodyActual = window.document.body;
+  const bodyExpected = new jsdom.JSDOM(file('slider-horizontal.html')).window
     .document.body;
 
-  d3.select(bodyActual)
-    .select('g')
-    .call(d3.sliderHorizontal());
+  select(bodyActual).select('g').call(d3.sliderHorizontal());
 
-  test.equal(bodyActual.outerHTML, bodyExpected.outerHTML);
-  test.end();
+  assert.equal(bodyActual.outerHTML, bodyExpected.outerHTML);
 });
 
-tape('sliderHorizontal(selection) produces the expected result', function(
-  test
-) {
-  var window = new jsdom.JSDOM('<!DOCTYPE html><svg><g></g></svg>').window;
+it('sliderVertical(selection) produces the expected result', () => {
+  const window = new jsdom.JSDOM('<!DOCTYPE html><svg><g></g></svg>').window;
   global.window = window;
   global.document = window.document;
   global.navigator = {
     userAgent: 'node.js',
   };
   copyProps(window, global);
-  var bodyActual = window.document.body;
-  var bodyExpected = new jsdom.JSDOM(file('slider-vertical.html')).window
+  const bodyActual = window.document.body;
+  const bodyExpected = new jsdom.JSDOM(file('slider-vertical.html')).window
     .document.body;
 
-  d3.select(bodyActual)
-    .select('g')
-    .call(d3.sliderVertical());
+  select(bodyActual).select('g').call(d3.sliderVertical());
 
-  test.equal(bodyActual.outerHTML, bodyExpected.outerHTML);
-  test.end();
+  assert.equal(bodyActual.outerHTML, bodyExpected.outerHTML);
 });
 
 function file(file) {
-  return fs
-    .readFileSync(path.join(__dirname, file), 'utf8')
-    .replace(/\n\s*/gm, '');
+  const __dirname = resolve(
+    dirname(fileURLToPath(new URL('./test', import.meta.url)))
+  );
+
+  return readFileSync(join(__dirname, file), 'utf8').replace(/\n\s*/gm, '');
 }
 
 function copyProps(src, target) {
